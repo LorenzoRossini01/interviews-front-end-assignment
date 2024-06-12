7077d388029f7b368668c9a5f6062089a70011c3
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-12 14:24:51 +0200

    feat: add Pagination to recipeList

--- src/components/AppMainRecipePage.vue
+++ src/components/AppMainRecipePage.vue
@@ -13,6 +13,9 @@ export default {
       difficulties: [],
       diets: [],
       filtersActive: false,
+
+      currentPage: 1, // Pagina corrente
+      recipesPerPage: 4, // Numero di ricette per pagina
     };
   },
 
@@ -21,35 +24,66 @@ export default {
     RecipeFilter,
   },
 
+  computed: {
+    // Calcola le ricette da visualizzare sulla pagina corrente
+    paginatedRecipes() {
+      const start = (this.currentPage - 1) * this.recipesPerPage;
+      const end = start + this.recipesPerPage;
+      return this.recipes.slice(start, end);
+    },
+    // Calcola il numero totale di pagine
+    totalPages() {
+      return Math.ceil(this.recipes.length / this.recipesPerPage);
+    },
+  },
+
   methods: {
     fetchRecipes() {
       axios.get(api.recipes).then((response) => {
-        console.log(response);
+        // console.log(response.data);
         this.recipes = response.data;
       });
     },
 
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    // Metodo per andare alla pagina precedente
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    // Metodo per andare a una pagina specifica
+    goToPage(page) {
+      if (page >= 1 && page <= this.totalPages) {
+        this.currentPage = page;
+      }
+    },
+
     fetchComments() {
       axios.get(api.comments).then((response) => {
-        console.log(response);
+        // console.log(response);
         this.comments = response.data;
       });
     },
     fetchCuisines() {
       axios.get(api.cuisines).then((response) => {
-        console.log(response);
+        // console.log(response);
         this.cuisines = response.data;
       });
     },
     fetchDifficulties() {
       axios.get(api.difficulties).then((response) => {
-        console.log(response);
+        // console.log(response);
         this.difficulties = response.data;
       });
     },
     fetchDiets() {
       axios.get(api.diets).then((response) => {
-        console.log(response);
+        // console.log(response);
         this.diets = response.data;
       });
     },
@@ -74,7 +108,7 @@ export default {
     <div class="row justify-content-center g-3">
       <div :class="filtersActive ? 'col-8' : 'col-12'">
         <RecipeList
-          :recipes="recipes"
+          :recipes="paginatedRecipes"
           :comments="comments"
           :cuisines="cuisines"
           :diets="diets"
@@ -82,6 +116,26 @@ export default {
           :filtersActive="filtersActive"
           @openFilterCard="handleFilterClick()"
         />
+        <div class="pagination-container d-flex justify-content-end">
+          <div class="pagination">
+            <button @click="previousPage" :disabled="currentPage === 1">
+              Previous
+            </button>
+
+            <button
+              v-for="page in totalPages"
+              :key="page"
+              @click="goToPage(page)"
+              :class="{ active: page === currentPage }"
+            >
+              {{ page }}
+            </button>
+
+            <button @click="nextPage" :disabled="currentPage === totalPages">
+              Next
+            </button>
+          </div>
+        </div>
       </div>
       <div :class="filtersActive ? 'col-4' : ''">
         <RecipeFilter
@@ -98,4 +152,29 @@ export default {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  margin-top: 1rem;
+
+  button {
+    padding: 0.5rem 1rem;
+    border: none;
+    background-color: #ddd;
+    cursor: pointer;
+
+    &.active {
+      background-color: #333;
+      color: #fff;
+    }
+
+    &:disabled {
+      background-color: #bbb;
+      cursor: not-allowed;
+    }
+  }
+}
+</style>
